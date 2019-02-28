fe90f259f176c81ce50f54fbf0b4e0b7ffc6cc21
David Paiva <hello@c2g.space>
2019-02-28 14:28:03 +0000

    Update CurrencyConverter.kt

--- src/main/kotlin/net/mrgaabriel/currencyconverter/CurrencyConverter.kt
+++ src/main/kotlin/net/mrgaabriel/currencyconverter/CurrencyConverter.kt
@@ -48,45 +48,4 @@ object CurrencyConverter {
         return value * quantity
     }
 
-    /**
-     * Converts quantity from a currency to another
-     *
-     * @param quantity = Quantity of FROM currency to be converted
-     * @param from = FROM currency
-     * @param to = TO currency
-     *
-     * @sample convert(1, "USD". "BRL")
-     * @throws CurrencyConvertException if any currency is not valid or if the response code of the API is not OK
-     *
-     * @return result of the conversion
-     */
-    fun convert(quantity: Int, from: String, to: String): Double {
-        val fromCurrency = CurrencyConverterUtils.getCurrencyCodes().firstOrNull { it == from }
-        val toCurrency = CurrencyConverterUtils.getCurrencyCodes().firstOrNull { it == to }
-
-        if (fromCurrency == null)
-            throw CurrencyConvertException("\"${from.toUpperCase()}\" is not a valid currency code!")
-
-        if (toCurrency == null)
-            throw CurrencyConvertException("\"${to.toUpperCase()}\" is not a valid currency code!")
-
-        val url = "https://api.exchangeratesapi.io/latest?base=$fromCurrency&symbols=$toCurrency"
-
-        val request = HttpRequest.get(url)
-            .accept("application/json")
-            .userAgent("Mozilla/5.0 (X11; Linux x86_64; rv:65.0) Gecko/20100101 Firefox/65.0")
-
-        if (!request.ok())
-            throw CurrencyConvertException("Request is not OK! Request body : ${request.body()}")
-
-        val body = request.body()
-        val payload = JsonParser().parse(body).obj
-
-        val rates = payload["rates"].obj
-        val value = rates[toCurrency].double
-
-        return value
-    }
-
-
 }
